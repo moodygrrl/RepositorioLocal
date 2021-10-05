fe41f3af781bd28b0aca7359aede3cb78b2206e5
moodygrrl <rebeca.criado@educa.madrid.org>
2021-10-05 18:52:17 +0200

    Primera versión del proyecto

---
+++ PrimeraTarea.java
@@ -0,0 +1,7 @@
+public class PrimeraTarea {
+    public static void main(String[] args) {
+        System.out.println("Hola, mundo");
+        System.out.println("Módulo: Entornos de Desarrollo");
+		System.out.println("Esto lo añado para ver si GIT recoge los cambios");
+    }
+}
