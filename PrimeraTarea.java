9fc9faa3634a2faf1e6a95841808f6417c255002
moodygrrl <rebeca.criado@educa.madrid.org>
2021-10-13 19:18:27 +0200

    SEGUNDAversion

--- PrimeraTarea.java
+++ PrimeraTarea.java
@@ -3,5 +3,6 @@ public class PrimeraTarea {
         System.out.println("Hola, mundo");
         System.out.println("M�dulo:  Entornos de Desarrollo");
 		System.out.println("Esto lo a�ado para ver si GIT recoge los cambios");
+        System.out.println("He conseguido sincronizarlo con el remoto :_");
     }
 }
