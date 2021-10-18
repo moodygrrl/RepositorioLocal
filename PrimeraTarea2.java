1d56181e4b54b0cca0b37d17137242c60b270e89
moodygrrl <rebeca.criado@educa.madrid.org>
2021-10-18 16:30:22 +0200

    PrimeraTarea2

---
+++ PrimeraTarea2.java
@@ -0,0 +1,9 @@
+public class PrimeraTarea {
+    public static void main(String[] args) {
+        System.out.println("Hola, mundo");
+        System.out.println("M�dulo:  Entornos de Desarrollo");
+		System.out.println("Esto lo a�ado para ver si GIT recoge los cambios");
+        System.out.println("He conseguido sincronizarlo con el remoto :_");
+        System.out.println("Sara ha clonado el repositorio :__");
+    }
+}
